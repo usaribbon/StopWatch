762d877be88cff1ea27d1533a87572ec5e8bd9ba
usaribbon <me@usaribbon.net>
2013-06-21 16:03:25 +0900

    true prg

--- practiceRuby/AppDelegate.rb
+++ practiceRuby/AppDelegate.rb
@@ -9,9 +9,7 @@
 
 class AppDelegate
     attr_accessor :window
-    attr_accessor :window
-    attr_accessor :textField
-    
+    attr_accessor :textField # Outlet
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
     end
@@ -19,26 +17,25 @@ class AppDelegate
     def startTimer(sender)
         if @timer.nil?
             @time = 0.0
-            @timer = NSTimer.scheduledTimerWithTimeIntercal(0.1,
-                                                            target: seld
-            @timer = NSTimer.scheduledTimerWithTimeIntercal(0.1,
-                                                            target: self,
-                                                            selector: "timerHandler",
-                                                            userInfo: nil,
-                                                            repeats: true)
+            @timer = NSTimer
+            .scheduledTimerWithTimeInterval(0.1,
+                                            target: self,
+                                            selector: "timerHandler:",
+                                            userInfo: nil,
+                                            repeats: true)
         end
     end
     
     def stopTimer(sender)
         if @timer
-            @timer.initialize
+            @timer.invalidate
             @timer = nil
         end
     end
-   
+    
     def timerHandler(userInfo)
         @time += 0.1
-        string = sprintf("%.1f",@time)
+        string = sprintf("%.1f", @time)
         textField.setStringValue(string)
     end
 end
